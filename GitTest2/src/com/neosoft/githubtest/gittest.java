e93ed8eea8b75150a4a1470c739ee0109bb5b371
khansaif7887 <ksaif3606@gmail.com>
2021-07-16 18:57:38 +0530

    first

---
+++ GitTest2/src/com/neosoft/githubtest/gittest.java
@@ -0,0 +1,14 @@
+package com.neosoft.githubtest;
+
+public class gittest {
+
+	public static void main(String[] args) {
+		String code= "SE";
+		if (code == "SE")
+			System.out.println("Software Engineer");
+		else
+			System.out.println("not a SE");
+
+	}
+
+}
